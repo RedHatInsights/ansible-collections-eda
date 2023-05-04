c6c61f05ac07cc8a8d8f0b14b54a918b0764ce7c
Viliam Krizan <vkrizan@redhat.com>
2023-05-04 16:55:07 +0200

    feat: add token authentication support
    Adds support for token based authentication through:
    * X-Insight-Token header,
    * or, Authorization Bearer header
    
    EVNT-853

--- tests/unit/event_source/test_insights.py
+++ tests/unit/event_source/test_insights.py
@@ -9,11 +9,11 @@ async def start_server(queue, args):
     await insights_main(queue, args)
 
 
-async def post_code(server_task, args, payload):
+async def post_code(server_task, args, payload, headers=None):
     url = f'http://{args["host"]}:{args["port"]}/endpoint'
 
     async with aiohttp.ClientSession() as session:
-        async with session.post(url, json=payload):
+        async with session.post(url, json=payload, headers=headers):
             pass
 
     server_task.cancel()
@@ -36,21 +36,36 @@ async def test_cancel():
 
 
 @pytest.mark.asyncio
-async def test_post_endpoint():
+@pytest.mark.parametrize(
+    "token,auth_header",
+    [
+        pytest.param(None, None, id="no_token"),
+        pytest.param("secret", "Authorization", id="bearer_token"),
+        pytest.param("secret", "X-Insight-Token", id="x_insights_token"),
+    ],
+)
+async def test_post_endpoint(token, auth_header):
     queue = asyncio.Queue()
 
     args = {"host": "127.0.0.1", "port": 8000}
+    if token:
+        args["token"] = token
+
     plugin_task = asyncio.create_task(start_server(queue, args))
+
+    headers = {}
+    if auth_header:
+        headers[auth_header] = f"Bearer {token}" if auth_header == "Authorization" else token
 
     payload = {
         "eventdata": "insights"
     }
 
-    post_task = asyncio.create_task(post_code(plugin_task, args, payload))
+    post_task = asyncio.create_task(post_code(plugin_task, args, payload, headers=headers))
 
     await asyncio.gather(plugin_task, post_task)
 
-    data = await queue.get()
+    data = await asyncio.wait_for(queue.get(), 1)
     assert "payload" in data
     assert "meta" in data
     assert "eventdata" in data["payload"]
